bc1d2092ecc65ca358e30aaaf4d8e6b3962a9c7f
evilimas <eg.vilimas@gmail.com>
2023-08-10 18:51:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+//HOME score
+let homeScoreEl = document.getElementById("home-score");
+let homeScore = 0;
+
+function increaseHomeScoreByOne(){
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+    winner()
+}
+function increaseHomeScoreByTwo(){
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+    winner()
+}
+function increaseHomeScoreByThree(){
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+    winner()
+}
+
+//GUEST score
+let guestScoreEl = document.getElementById("guest-score")
+let guestScore = 0
+
+function increaseGuestScoreByOne(){
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+    winner()
+}
+function increaseGuestScoreByTwo(){
+    guestScore += 2
+    guestScoreEl.textContent = guestScore
+    winner()
+}
+function increaseGuestScoreByThree(){
+    guestScore += 3
+    guestScoreEl.textContent = guestScore
+    winner()
+}
+
+//New game
+let newGameBtn = document.getElementById("new-game")
+
+newGameBtn.addEventListener("click", function(){
+    homeScore = 0
+    guestScore = 0
+    guestScoreEl.textContent = guestScore
+    homeScoreEl.textContent = homeScore
+})
+//Wining team
+function winner(){
+    if(homeScore > guestScore){
+        homeScoreEl.style.border = "2px solid #0000FF"
+        guestScoreEl.style.border = "none"
+
+    } else if(guestScore > homeScore) {
+        guestScoreEl.style.border = "2px solid #0000FF"
+        homeScoreEl.style.border = "none"
+
+    } else{
+        homeScoreEl.style.border = "none"
+        guestScoreEl.style.border = "none"
+    }
+
+}
